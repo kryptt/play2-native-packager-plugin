9a77d1efb1f4cd9811a6c5f6f2c74592b5546234
Rodolfo Hansen <rhansen@kitsd.com>
2012-06-27 18:31:14 -0400

    blank Plugin and Command

---
+++ src/main/scala/net/kindleit/play2/natpackplugin/NatPackCommands.scala
@@ -0,0 +1,7 @@
+package net.kindleit.play2.natpackplugin
+
+import sbt._
+
+trait NatPackCommands extends PlayCommands with PlayReloader {
+
+}
---
+++ src/main/scala/net/kindleit/play2/natpackplugin/NatPackPlugin.scala
@@ -0,0 +1,7 @@
+package net.kindleit.play2.natpackplugin
+
+import sbt._
+
+object NatPackPlugin extends Plugin with NatPackCommands {
+
+}
