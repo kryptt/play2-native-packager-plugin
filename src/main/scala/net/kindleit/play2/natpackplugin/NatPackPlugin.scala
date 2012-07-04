ed42edc673cc6a428bba0adc8a838d833f77e435
Rodolfo Hansen <rhansen@kitsd.com>
2012-07-04 17:58:10 -0400

    Finaly got the thing right.

--- src/main/scala/net/kindleit/play2/natpackplugin/NatPackPlugin.scala
+++ src/main/scala/net/kindleit/play2/natpackplugin/NatPackPlugin.scala
@@ -1,34 +1,66 @@
 package net.kindleit.play2.natpackplugin
 
 import sbt._
+import com.typesafe.packager._
+import com.typesafe.packager.Keys._
+import sbt.Keys._
+import String.format
 
-object NatPackPlugin extends Plugin {
+object NatPackPlugin extends Plugin with debian.DebianPlugin {
 
-  object NatPackKeys extends NatPackKeys {
-
-    lazy val debian = TaskKey[File]("debian", "Generate the debian native package")
-
-    lazy val maintainer = SettingKey[String]("package-maintainer", "Package maintainer for the generated native package")
-    lazy val summary = SettingKey[String]("package-summary", "Package summary for the generated native package")
-    lazy val description = SettingKey[String]("package-description", "Package description for the generated native package")
-
+  object NatPackKeys extends linux.Keys with debian.DebianKeys {
+    lazy val debian = TaskKey[File]("deb", "Create the debian package")
   }
-
-  private object DebPackage extends NatPackCommands {
-    override def pkgType = "deb"
-  }
-
   private val npkg = NatPackKeys
 
-  lazy val natPackSettings: Seq[Project.Setting[_]] = Seq(
-    npkg.maintainer := "Unknown Maintainer",
-    npkg.summary <<= Keys.description,
-    npkg.description <<= (Keys.description, Keys.homepage) { (desc, url) =>
-      String.format("%s.\n%s", url, desc)
+  lazy val natPackSettings: Seq[Project.Setting[_]] = linuxSettings ++ debianSettings ++ Seq(
+    name in Debian <<= normalizedName,
+    version in Debian <<= version,
+    packageDescription <<= description,
+    packageSummary <<= description,
+
+    debianPackageDependencies in Debian += "java2-runtime",
+    debianPackageRecommends in Debian += "git",
+
+    linuxPackageMappings <++=
+      (baseDirectory, sourceDirectory, target, dependencyClasspath in Runtime, PlayProject.playPackageEverything, normalizedName, version) map {
+      (root, sd, target, dependencies, packages, name, v) ⇒
+        val pkgName = format("%s-%s", name, v)
+        val start = target / "start"
+        writeStartFile(start)
+
+        packages.map { pkg =>
+          packageMapping(pkg -> format("/opt/%s/%s", pkgName, pkg.getName)) withPerms "0644"
+        } ++
+        dependencies.filter(_.data.ext == "jar").map { dependency ⇒
+          val depFilename = dependency.metadata.get(AttributeKey[ModuleID]("module-id")).map { module ⇒
+            module.organization + "." + module.name + "-" + module.revision + ".jar"
+          }.getOrElse(dependency.data.getName)
+          packageMapping(dependency.data -> format("/opt/%s/lib/%s", pkgName, depFilename)) withPerms "0644"
+        } ++
+        (config map { cfg ⇒ packageMapping(root / cfg -> format("/opt/%s/application.conf", pkgName)) }) ++
+          Seq(packageMapping(
+            start -> format("/opt/%s/start", pkgName),
+            root / "README" -> format("/opt/%s/README", pkgName)
+        ))
     },
-    npkg.pkgMeta <<= (npkg.maintainer, npkg.summary, npkg.description) { (maint, summ, desc) ⇒
-      NatPackKeys.PackageMetaData(maint, summ, desc)
-    },
-    npkg.debian <<= DebPackage.pkgTask
-  )
+
+
+    npkg.debian <<= (packageBin in Debian, streams) map  { (deb, s) =>
+      s.log.info(format("Package %s ready", deb))
+      s.log.info(format("If you wish to sign the package as well, run %s:%s", Debian, debianSign.key))
+      deb
+    }
+
+  ) ++ SettingsHelper.makeDeploymentSettings(Debian, packageBin in Debian, "deb")
+
+
+  private val config = Option(System.getProperty("config.file"))
+
+  //local Play start file
+  private def writeStartFile(start: File) = IO.write(start, format(
+"""#!/usr/bin/env sh
+
+exec java $* -cp "`dirname $0`/lib/*" %s play.core.server.NettyServer `dirname $0`
+""", config.map(_ => "-Dconfig.file=`dirname $0`/application.conf ").getOrElse("")))
 }
