f3fee333814835268f2f4b6ef0981f0e73d4e557
Rodolfo Hansen <rhansen@kitsd.com>
2012-07-17 16:37:54 -0400

    Fixes #2 Modify user and group as Settings.

--- src/main/scala/net/kindleit/play2/natpackplugin/NatPackPlugin.scala
+++ src/main/scala/net/kindleit/play2/natpackplugin/NatPackPlugin.scala
@@ -13,6 +13,8 @@ object NatPackPlugin extends Plugin with debian.DebianPlugin {
     lazy val debianPreInst = TaskKey[File]("debian-preinst-file", "Debian pre install maintainer script")
     lazy val debianPreRm   = TaskKey[File]("debian-prerm-file", "Debian pre remove maintainer script")
     lazy val debianPostRm  = TaskKey[File]("debian-postrm-file", "Debian post remove maintainer script")
+    lazy val userName     = SettingKey[String]("Unix user to own the extracted package files")
+    lazy val groupName     = SettingKey[String]("Unix group to own the extracted package files")
   }
   private val npkg = NatPackKeys
 
@@ -26,28 +28,28 @@ object NatPackPlugin extends Plugin with debian.DebianPlugin {
     debianPackageRecommends in Debian += "git",
 
     linuxPackageMappings <++=
-      (baseDirectory, target, normalizedName, packageSummary, PlayProject.playPackageEverything, dependencyClasspath in Runtime) map {
-      (root, target, name, desc, pkgs, deps) ⇒
+      (baseDirectory, target, normalizedName, npkg.userName, npkg.groupName, packageSummary, PlayProject.playPackageEverything, dependencyClasspath in Runtime) map {
+      (root, target, name, usr, grp, desc, pkgs, deps) ⇒
         val start = target / "start"
         val init = target / "initFile"
         IO.write(start, startFileContent)
         IO.write(init, initFilecontent(name, desc))
 
         pkgs.map { pkg ⇒
-          packageMapping(pkg -> format("/var/lib/%s/%s", name, pkg.getName)) withUser(name) withGroup("adm")
+          packageMapping(pkg -> format("/var/lib/%s/%s", name, pkg.getName)) withUser(usr) withGroup(grp)
         } ++
         deps.filter(_.data.ext == "jar").map { dependency ⇒
           val depFilename = dependency.metadata.get(AttributeKey[ModuleID]("module-id")).map { module ⇒
             module.organization + "." + module.name + "-" + module.revision + ".jar"
           }.getOrElse(dependency.data.getName)
-          packageMapping(dependency.data -> format("/var/lib/%s/lib/%s", name, depFilename)) withUser(name) withGroup("adm") withPerms("0644")
+          packageMapping(dependency.data -> format("/var/lib/%s/lib/%s", name, depFilename)) withUser(usr) withGroup(grp) withPerms("0644")
         } ++
         (config map { cfg ⇒
-          packageMapping(root / cfg -> format("/var/lib/%s/application.conf", name)) withUser(name) withGroup("adm") withPerms("0644")
+          packageMapping(root / cfg -> format("/var/lib/%s/application.conf", name)) withUser(usr) withGroup(grp) withPerms("0644")
         }) ++ Seq(
-          packageMapping(start -> format("/var/lib/%s/start", name)) withUser(name) withGroup("adm"),
+          packageMapping(start -> format("/var/lib/%s/start", name)) withUser(usr) withGroup(grp),
           packageMapping(init -> format("/etc/init.d/%s/", name)) withConfig(),
-          packageMapping(root / "README" -> format("/var/lib/%s/README", name)) withUser(name) withGroup("adm") withPerms("0644")
+          packageMapping(root / "README" -> format("/var/lib/%s/README", name)) withUser(usr) withGroup(grp) withPerms("0644")
         )
     },
 
@@ -57,7 +59,8 @@ object NatPackPlugin extends Plugin with debian.DebianPlugin {
       deb
     }
   ) ++ inConfig(Debian)(Seq(
-
+    npkg.userName         <<= normalizedName,
+    npkg.groupName        <<= npkg.userName,
     npkg.debianPreInst    <<= (target, normalizedName) map debFile("postinst", postInstContent),
     npkg.debianPreRm      <<= (target, normalizedName) map debFile("prerm", preRmContent),
     npkg.debianPostRm     <<= (target, normalizedName) map debFile("postrm", postRmContent),
