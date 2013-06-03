c40c31ddc193cb10bcb799eb2aff061a20802837
Rodolfo Hansen <rhansen@kitsd.com>
2013-06-03 13:48:11 -0500

    Re-organizing some details

--- src/main/scala/net/kindleit/play2/natpackplugin/NatPackPlugin.scala
+++ src/main/scala/net/kindleit/play2/natpackplugin/NatPackPlugin.scala
@@ -11,24 +11,23 @@ object NatPackPlugin extends Plugin with debian.DebianPlugin {
   object NatPackKeys extends linux.Keys with debian.DebianKeys {
     lazy val debian        = TaskKey[File]("deb", "Create the debian package")
     lazy val debianPreInst = TaskKey[File]("debian-preinst-file", "Debian pre install maintainer script")
-    lazy val debianPreRm   = TaskKey[File]("debian-prerm-file", "Debian pre remove maintainer script")
-    lazy val debianPostRm  = TaskKey[File]("debian-postrm-file", "Debian post remove maintainer script")
-    lazy val userName     = SettingKey[String]("Unix user to own the extracted package files")
+    lazy val debianPreRm   = TaskKey[File]("debian-prerm-file",   "Debian pre remove maintainer script")
+    lazy val debianPostRm  = TaskKey[File]("debian-postrm-file",  "Debian post remove maintainer script")
+    lazy val userName      = SettingKey[String]("Unix user to own the extracted package files")
     lazy val groupName     = SettingKey[String]("Unix group to own the extracted package files")
   }
   private val npkg = NatPackKeys
 
   lazy val natPackSettings: Seq[Project.Setting[_]] = linuxSettings ++ debianSettings ++ Seq(
 
-    name in Debian <<= normalizedName,
-    version in Debian <<= version,
+    name               in Debian <<= normalizedName,
+    version            in Debian <<= version,
+    packageSummary     in Debian <<= description,
     packageDescription in Debian <<= description,
-    packageSummary <<= description,
-    debianPackageDependencies in Debian ++= Seq("java2-runtime", "daemon"),
-    debianPackageRecommends in Debian += "git",
 
     linuxPackageMappings <++=
-      (baseDirectory, target, normalizedName, npkg.userName, npkg.groupName, packageSummary, PlayProject.playPackageEverything, dependencyClasspath in Runtime) map {
+      (baseDirectory, target, normalizedName, npkg.userName, npkg.groupName, packageSummary in Debian,
+       PlayProject.playPackageEverything, dependencyClasspath in Runtime) map {
       (root, target, name, usr, grp, desc, pkgs, deps) ⇒
         val start = target / "start"
         val init = target / "initFile"
@@ -52,19 +51,20 @@ object NatPackPlugin extends Plugin with debian.DebianPlugin {
           packageMapping(root / "README" -> format("/var/lib/%s/README", name)) withUser(usr) withGroup(grp) withPerms("0644")
         )
     },
-
     npkg.debian <<= (packageBin in Debian, streams) map { (deb, s) ⇒
       s.log.info(format("Package %s ready", deb))
       s.log.info(format("If you wish to sign the package as well, run %s:%s", Debian, debianSign.key))
       deb
     }
   ) ++ inConfig(Debian)(Seq(
-    npkg.userName         <<= normalizedName,
-    npkg.groupName        <<= npkg.userName,
-    npkg.debianPreInst    <<= (target, normalizedName) map debFile("postinst", postInstContent),
-    npkg.debianPreRm      <<= (target, normalizedName) map debFile("prerm", preRmContent),
-    npkg.debianPostRm     <<= (target, normalizedName) map debFile("postrm", postRmContent),
-    debianExplodedPackage <<= debianExplodedPackage.dependsOn(npkg.debianPreInst, npkg.debianPreRm, npkg.debianPostRm)
+    npkg.userName             <<= normalizedName,
+    npkg.groupName            <<= npkg.userName,
+    npkg.debianPreInst        <<= (target, normalizedName) map debFile("postinst", postInstContent),
+    npkg.debianPreRm          <<= (target, normalizedName) map debFile("prerm", preRmContent),
+    npkg.debianPostRm         <<= (target, normalizedName) map debFile("postrm", postRmContent),
+    debianExplodedPackage     <<= debianExplodedPackage.dependsOn(npkg.debianPreInst, npkg.debianPreRm, npkg.debianPostRm),
+    debianPackageDependencies ++= Seq("java2-runtime", "daemon"),
+    debianPackageRecommends    += "git"
 
   )) ++
   SettingsHelper.makeDeploymentSettings(Debian, packageBin in Debian, "deb")
